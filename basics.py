1d5986f1fbad9d50b048e4d64d31a81d3f00c64c
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2023-01-05 17:29:15 +0530

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,14 @@
+import cv2
+image = cv2.imread('img.jpg', 0) #for reading an image file
+
+img=cv2.resize(image, (500, 500)) #resizing them if they are too large
+
+img=cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE) #rotating them in 90 degree
+
+cv2.imshow('Image', img) #displaying the image
+
+cv2.waitKey(0) #press any key to close the image
+
+cv2.destroyAllWindows()
+
+cv2.imwrite('img_1.jpg', img) #creating a new image file with the exiting one
